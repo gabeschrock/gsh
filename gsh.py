56b279c74a7e662c410565e0c99f8cc68289b681
Gabe Schrock <gabeschrock@gmail.com>
2022-12-29 14:59:08 -0500

    push latest changes

--- gsh.py
+++ gsh.py
@@ -1,52 +1,72 @@
-def findQuote(char, quote, inQuotes, esc):
+def findQuote(char, quote, esc):
     '''not meant for usage by the user'''
-    if char == quote and not esc:
-        return not inQuotes
-def Tokenize(string):
+    return char == quote and not esc
+def Tokenize(string:str, vars:dict) -> list:
     '''not meant for usage by the user'''
     Quotes = 3
     Esc = False
     Token = ""
-    Tokens = {}
-    for char in list(string):
-        if findQuote(char, "'", False, Esc or Quotes == 3):
+    Tokens = []
+    ind = 0
+    var = ""
+    string = f"{string} "
+    while not ind >= len(string) - 1:
+        if findQuote(string[ind], "$", Esc or Quotes == 1):
+            ind += 1
+            if string[ind].isalpha():
+                while string[ind].isalpha():
+                    var += string[ind]
+                    ind += 1
+                try:
+                    Token += vars[var]
+                except Exception:
+                    print(f"Error: variable {var} does not exist")
+                    return ""
+                var = ""
+            else:
+                Token += "$"
+        if findQuote(string[ind], "'", Esc or Quotes == 2):
             if Quotes == 3:
                 Quotes = 1
                 if Token != "":
-                    Tokens[len(Tokens) + 1] = [Token, False]
+                    Tokens.append(Token)
             else:
                 Quotes = 3
-                Tokens[len(Tokens) + 1] = [f"'{Token}'", True]
+                Tokens.append(Token)
             Token = ""
-        elif findQuote(char, '"', False, Esc or Quotes == 1):
+        elif findQuote(string[ind], '"', Esc or Quotes == 1):
             if Quotes == 3:
                 Quotes = 2
                 if Token != "":
-                    Tokens[len(Tokens) + 1] = [Token, False]
+                    Tokens.append(Token)
             else:
                 Quotes = 3
-                Tokens[len(Tokens) + 1] = [f'"{Token}"', True]
+                Tokens.append(Token)
             Token = ""
-        elif char.isspace() and Quotes == 3 and not Esc:
+        elif string[ind].isspace() and Quotes == 3 and not Esc:
             if Token != "":
-                Tokens[len(Tokens) + 1] = [Token, False]
+                Tokens.append(Token)
             Token = ""
-        elif (char != "\\") or (char == "\\" and Esc):
-            Token  += char
-        if Esc or (char == "\\" and not Esc):
+        elif Esc and string[ind].lower() == "n":
+            Token += "\n"
+        elif (string[ind] != "\\") or (string[ind] == "\\" and Esc):
+            Token  += string[ind]
+        if Esc or (string[ind] == "\\" and not Esc):
             Esc = not Esc
+        ind += 1
     if Quotes != 3:
+        print("Error: quote left open")
         return ""
     else:
         if Token != "":
-            Tokens[len(Tokens) + 1] = [Token, False]
+            Tokens.append(Token)
         return Tokens
 
 def main():
     cont = True
     while cont:
         try:
-            print(Tokenize(input("gsh> ")))
+            print(Tokenize(input("gsh> "), {"foo": "bar", "bar": "baz", "baz": "foo"}))
         except EOFError:
             print("^D")
             cont = False
